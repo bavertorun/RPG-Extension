6b46a65206a0049573fdc723571a34c7029170d7
Baver Torun <bavertorunx@gmail.com>
2024-03-15 18:26:11 +0300

    Project Finish

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# This project is still under development.
+# Random Password Generator Extension
--- rpg.js
+++ rpg.js
@@ -4,3 +4,43 @@
  * Github Repo: https://github.com/bavertorun/RPG-Extension
  **/
 
+const length = document.getElementById("length");
+const lenLabel = document.getElementById("lenLabel");
+const randInput = document.getElementById("randInput");
+const copyBtn = document.getElementById("copyBtn");
+const refresh = document.getElementById("refresh");
+
+let len;
+
+length.addEventListener("change", (e) => {
+  len = e.target.value;
+  console.log(len);
+  lenLabel.innerHTML = "Password Length: " + e.target.value;
+  generatePassword(len);
+});
+
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(randInput.value);
+  copyBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+  setInterval(() => {
+    copyBtn.innerHTML = '<i class="fa-solid fa-copy"></i>';
+  }, 1500);
+});
+
+
+const generatePassword = (len) => {
+  const characters =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()+_-=}{[]|:;"/?.><,`~';
+  let pass = "";
+
+  for (let i = 0; i < len; i++) {
+    pass += characters.charAt(Math.floor(Math.random() * characters.length));
+    console.log(pass);
+  }
+
+  randInput.value = pass;
+};
+
+refresh.addEventListener("click", ()=>{
+    generatePassword(len)
+});
